bcf4283913beb3eec24afc1de62b49b8424abc61
uday-venkatesha <udayvenkatesh2015@gmail.com>
2024-10-17 15:21:05 -0500

    pickle file created

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,19 +1,15 @@
 import sys
 from dataclasses import dataclass
-
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
-
 from src.exception import CustomException
 from src.logger import logging
 import os
-
 from src.utils import save_object
-
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
@@ -24,7 +20,7 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
